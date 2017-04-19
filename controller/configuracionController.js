c5f72a3e7f1183ba4e5c368d06a4295a7cf9a791
PangeaBogota <Pangeabogota@hotmail.com>
2017-04-19 09:14:11 -0500

    1231
    13123

---
+++ controller/configuracionController.js
@@ -0,0 +1,16 @@
+app_angular.controller("configController",['Conexion','$scope','$route',function (Conexion,$scope,$route) {
+	
+	$scope.sincronizacion=window.localStorage.getItem("TIPO_SINCRONIZACION")
+	if ($scope.sincronizacion==null || $scope.sincronizacion==undefined || $scope.sincronizacion==NaN) {
+		$scope.sincronizacion='MANUAL';
+	}
+	$scope.$watch('sincronizacion',function(a,b){
+		//TIPO_SINCRONIZACION=$scope.sincronizacion;
+	})
+	$scope.Guardar=function()
+	{
+		localStorage.removeItem('TIPO_SINCRONIZACION');
+		localStorage.setItem('TIPO_SINCRONIZACION',$scope.sincronizacion); 
+		Mensajes('Guardado Correctamente','success');
+	}
+}]);
